1a905d9677549d9672a92db5e3946e5c9189aa81
s-fletcher <sfletchertx@gmail.com>
2020-08-29 18:31:23 -0500

    Basic footer implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
+import Footer from './components/Footer';
 
 /**
  * Top level of App to organize components
@@ -9,6 +10,7 @@ const App = () => (
   <div className="app">
     <Header />
     <About />
+    <Footer />
   </div>
 );
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/footer.scss';
+
+/**
+ * Contains all the footer links
+ */
+const Footer = () => (
+  <div className="footer">
+    <div className="container">
+      <p>Footer</p>
+    </div>
+  </div>
+);
+
+export default Footer;
