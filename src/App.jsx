c0da1c44be8eee4e4d9799a3b5fc65d1b90d2d8c
s-fletcher <sfletchertx@gmail.com>
2020-08-29 18:13:01 -0500

    About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import About from './components/About';
 
 /**
  * Top level of App to organize components
@@ -7,6 +8,7 @@ import Header from './components/Header';
 const App = () => (
   <div className="app">
     <Header />
+    <About />
   </div>
 );
 
---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/about.scss';
+
+/**
+ * Contains all the header needs
+ */
+const About = () => (
+  <div className="about">
+    <div className="box">
+      <h2>Hacklahoma is Oklahoma’s biggest Hackathon</h2>
+      <p><strong>Anyone who has an interest in technology</strong> attends a hackathon to learn, build and share their creations in a relaxed and collaborative atmosphere. Even those with little to no experience are welcome to attend and learn!</p>
+    </div>
+  </div>
+);
+
+export default About;
