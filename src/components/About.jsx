de86c639dbcb588d5ccea56771ea0511ee19e7a3
s-fletcher <sfletchertx@gmail.com>
2020-08-29 18:13:39 -0500

    Fix linting error

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,6 +8,7 @@ const About = () => (
   <div className="about">
     <div className="box">
       <h2>Hacklahoma is Oklahoma’s biggest Hackathon</h2>
+      {/* eslint-disable-next-line max-len */}
       <p><strong>Anyone who has an interest in technology</strong> attends a hackathon to learn, build and share their creations in a relaxed and collaborative atmosphere. Even those with little to no experience are welcome to attend and learn!</p>
     </div>
   </div>
