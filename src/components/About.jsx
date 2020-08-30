2fcf96de25425c41c3f65de3020c7f3229859003
s-fletcher <sfletchertx@gmail.com>
2020-08-30 14:18:17 -0500

    Touching up and rewording

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ import '../styles/about.scss';
 const About = () => (
   <div className="about">
     <div className="box">
-      <h2>Hacklahoma is Oklahoma’s biggest Hackathon</h2>
+      <h2>Hacklahoma is Oklahoma’s largest Hackathon</h2>
       {/* eslint-disable-next-line max-len */}
       <p><strong>Anyone who has an interest in technology</strong> attends a hackathon to learn, build and share their creations in a relaxed and collaborative atmosphere. Even those with little to no experience are welcome to attend and learn!</p>
     </div>
