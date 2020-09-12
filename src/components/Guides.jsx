fb0c1362138221978cbc10d1cebcd8eece1aff8f
s-fletcher <sfletchertx@gmail.com>
2020-09-11 19:48:48 -0500

    Coming soon for guides

--- src/components/Guides.jsx
+++ src/components/Guides.jsx
@@ -10,20 +10,20 @@ const Guides = () => (
     <div id="guides" className="guides">
       <h2>New to Hackathons?</h2>
       <div className="container">
-        <p className="description">Follow these guides and submit it as your first hack! We'll be there for you if you're having trouble.</p>
+        <p className="description">Follow these guides and submit it as your first hack! We'll be there for you if you're having trouble. <span>coming soon</span></p>
         <div className="tracks">
-          <a href="/" className="track">
+          <div href="#" className="track">
             <p className="head">Build a Website</p>
             <p className="body">Use popular JavaScript tools to create a full-stack web app.</p>
-          </a>
-          <a href="/" className="track">
+          </div>
+          <div href="#" className="track">
             <p className="head">Build a Desktop Application</p>
             <p className="body">Create a cross-platform desktop app using ElectronJS.</p>
-          </a>
-          <a href="/" className="track">
+          </div>
+          <div href="/" className="track">
             <p className="head">Build Bots</p>
             <p className="body">Use Python to create bots for different platforms.</p>
-          </a>
+          </div>
         </div>
       </div>
     </div>
