7f414eb74665d8f827fbf82285709edabc9002d3
s-fletcher <sfletchertx@gmail.com>
2021-01-04 23:14:14 -0600

    Update guides

--- src/components/Guides.jsx
+++ src/components/Guides.jsx
@@ -12,18 +12,18 @@ const Guides = () => (
       <div className="container">
         <p className="description">Follow these guides and submit it as your first hack! We'll be there for you if you're having trouble.</p>
         <div className="tracks">
-          <div href="https://github.com/Hacklahoma/guides/tree/react" className="track" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/Hacklahoma/guides/tree/react" className="track" target="_blank" rel="noopener noreferrer">
             <p className="head">Build a Website</p>
             <p className="body">Create a web application using the popular React framework.</p>
-          </div>
-          <div href="https://github.com/Hacklahoma/guides/tree/data-science" className="track" target="_blank" rel="noopener noreferrer">
+          </a>
+          <a href="https://github.com/Hacklahoma/guides/tree/data-science" className="track" target="_blank" rel="noopener noreferrer">
             <p className="head">Explore Data Science</p>
             <p className="body">Dive into basic applications of artificial intelligence and data science.</p>
-          </div>
-          <div href="https://github.com/Hacklahoma/guides/tree/bots" className="track" target="_blank" rel="noopener noreferrer">
+          </a>
+          <a href="https://github.com/Hacklahoma/guides/tree/bots" className="track" target="_blank" rel="noopener noreferrer">
             <p className="head">Build Bots</p>
             <p className="body">Use Python to create bots for different platforms.</p>
-          </div>
+          </a>
         </div>
       </div>
     </div>
