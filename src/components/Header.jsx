142720c8bfa7abbfdc1e3fb4db3ed8e826f49c1f
Shleby <shelby.a.huffy@gmail.com>
2021-01-18 12:29:07 -0600

    Updated registration date | Added paycom logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import '../styles/header.scss';
+import React from "react";
+import "../styles/header.scss";
 
 /**
  * Contains all the header needs
@@ -13,9 +13,21 @@ const Header = () => (
             <p>February 6-7, 2021</p>
             <h1>HACKLAHOMA 2021</h1>
           </div>
-          <p className="subtitle">Attend a 24 hour <strong>virtual</strong> innovation marathon with the <strong>University of Oklahoma</strong>!</p>
-          <a href="https://register.hacklahoma.org" target="_blank" rel="noopener noreferrer" className="register"><p>Register</p></a>
-          <p className="warning">Registration closes January 15th at 11:59 PM</p>
+          <p className="subtitle">
+            Attend a 24 hour <strong>virtual</strong> innovation marathon with
+            the <strong>University of Oklahoma</strong>!
+          </p>
+          <a
+            href="https://register.hacklahoma.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="register"
+          >
+            <p>Register</p>
+          </a>
+          <p className="warning">
+            Registration closes January 29th at 11:59 PM
+          </p>
         </div>
       </div>
     </div>
--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import '../styles/sponsors.scss';
-import Section from './Section';
+import React from "react";
+import "../styles/sponsors.scss";
+import Section from "./Section";
 
 /**
  * Contains all the header needs
@@ -9,35 +9,54 @@ const Sponsors = () => (
   <Section>
     <div id="sponsors" className="sponsors">
       <h2>Sponsors</h2>
-      <p>Interested in sponsoring? Check out our <a target="_blank" href="/sponsor_packet.pdf">sponsor packet</a>!</p>
+      <p>
+        Interested in sponsoring? Check out our{" "}
+        <a target="_blank" href="/sponsor_packet.pdf">
+          sponsor packet
+        </a>
+        !
+      </p>
       <div className="container">
         <a
           href="https://tailwindapp.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img alt="tailwind" src={require('../images/sponsors/tailwind.png')} />
+          <img
+            alt="tailwind"
+            src={require("../images/sponsors/tailwind.png")}
+          />
         </a>
         <a
           href="https://www.ou.edu/alumni/saa"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img alt="saa" src={require('../images/sponsors/saa.png')} />
+          <img alt="saa" src={require("../images/sponsors/saa.png")} />
         </a>
         <a
           href="https://www.ou.edu/sga"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img alt="sga" src={require('../images/sponsors/sga.png')} />
+          <img alt="sga" src={require("../images/sponsors/sga.png")} />
         </a>
         <a
           href="https://www.hobbylobby.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img alt="hobby lobby" src={require('../images/sponsors/hobbyLobby.png')} />
+          <img
+            alt="hobby lobby"
+            src={require("../images/sponsors/hobbyLobby.png")}
+          />
+        </a>
+        <a
+          href="https://www.paycom.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img alt="paycom" src={require("../images/sponsors/paycom.png")} />
         </a>
       </div>
     </div>
