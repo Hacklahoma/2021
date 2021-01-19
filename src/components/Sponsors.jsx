790788fc35dc425cea5bb13c7f8ccbaf0ed64169
s-fletcher <sfletchertx@gmail.com>
2021-01-19 14:36:14 -0600

    Update sponsors

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "../styles/sponsors.scss";
-import Section from "./Section";
+import React from 'react';
+import '../styles/sponsors.scss';
+import Section from './Section';
 
 /**
  * Contains all the header needs
@@ -11,64 +11,70 @@ const Sponsors = () => (
       <div id="sponsors" className="sponsors">
         <h2>Sponsors</h2>
         <p>
-          Interested in sponsoring? Check out our{" "}
+          Interested in sponsoring? Check out our{' '}
           <a target="_blank" href="/sponsor_packet.pdf">
             sponsor packet
           </a>
           !
         </p>
         <div className="container">
-          <a
-            href="https://tailwindapp.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              alt="tailwind"
-              src={require("../images/sponsors/tailwind.png")}
-            />
-          </a>
-          <a
-            href="http://www.flywheelenergy.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              alt="flywheel energy"
-              src={require("../images/sponsors/flywheelEnergy.png")}
-            />
-          </a>
-          <a
-            href="https://www.paycom.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img alt="paycom" src={require("../images/sponsors/paycom.png")} />
-          </a>
-          <a
-            href="https://www.ou.edu/alumni/saa"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img alt="saa" src={require("../images/sponsors/saa.png")} />
-          </a>
-          <a
-            href="https://www.ou.edu/sga"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img alt="sga" src={require("../images/sponsors/sga.png")} />
-          </a>
-          <a
-            href="https://www.hobbylobby.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              alt="hobby lobby"
-              src={require("../images/sponsors/hobbyLobby.png")}
-            />
-          </a>
+          <div className="tier tier1">
+            <a
+              href="https://tailwindapp.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                alt="tailwind"
+                src={require('../images/sponsors/tailwind.png')}
+              />
+            </a>
+          </div>
+          <div className="tier tier2">
+            <a
+              href="https://www.paycom.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img alt="paycom" src={require('../images/sponsors/paycom.png')} />
+            </a>
+            <a
+              href="http://www.flywheelenergy.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                alt="flywheel energy"
+                src={require('../images/sponsors/flywheelEnergy.png')}
+              />
+            </a>
+            <a
+              href="https://www.ou.edu/alumni/saa"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img alt="saa" src={require('../images/sponsors/saa.png')} />
+            </a>
+          </div>
+          <div className="tier tier3">
+            <a
+              href="https://www.hobbylobby.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                alt="hobby lobby"
+                src={require('../images/sponsors/hobbyLobby.png')}
+              />
+            </a>
+            <a
+              href="https://www.ou.edu/sga"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img alt="sga" src={require('../images/sponsors/sga.png')} />
+            </a>
+          </div>
         </div>
       </div>
     </Section>
@@ -76,19 +82,25 @@ const Sponsors = () => (
       <div id="partners" className="sponsors">
         <h2>Partners</h2>
         <div className="container">
-          <a
-            href="https://www.stickermule.com/unlock?ref_id=0539570701&utm_source=sponsorship&utm_campaign=mlh-sponsorship-2019&utm_medium=referral"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              alt="stickermule"
-              src={require("../images/sponsors/stickerMule.png")}
-            />
-          </a>
-          <a href="https://mlh.io" target="_blank" rel="noopener noreferrer">
-            <img alt="mlh" src={require("../images/sponsors/mlh.png")} />
-          </a>
+          <div className="tier tier2">
+            <a
+              href="https://www.stickermule.com/unlock?ref_id=0539570701&utm_source=sponsorship&utm_campaign=mlh-sponsorship-2019&utm_medium=referral"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                alt="stickermule"
+                src={require('../images/sponsors/stickerMule.png')}
+              />
+            </a>
+            <a
+              href="https://mlh.io"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img alt="mlh" src={require('../images/sponsors/mlh.png')} />
+            </a>
+          </div>
         </div>
       </div>
     </Section>
--- src/live/Live.jsx
+++ src/live/Live.jsx
@@ -4,6 +4,7 @@ import '../styles/live.scss';
 import Header from './Header';
 import CTA from './CTA';
 import Schedule from './Schedule';
+import Sponsors from '../components/Sponsors';
 
 /**
  * Top level of App to organize components
@@ -14,6 +15,7 @@ const Live = () => (
     <Header />
     <CTA />
     <Schedule />
+    <Sponsors />
   </div>
 );
 
