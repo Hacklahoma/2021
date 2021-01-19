3718c7790eff9eed7691743902c8b4e28fe3f9e6
Shleby <shelby.a.huffy@gmail.com>
2021-01-19 13:39:29 -0600

    Partners section created, mlh and sticker mule added

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -6,71 +6,93 @@ import Section from "./Section";
  * Contains all the header needs
  */
 const Sponsors = () => (
-  <Section>
-    <div id="sponsors" className="sponsors">
-      <h2>Sponsors</h2>
-      <p>
-        Interested in sponsoring? Check out our{" "}
-        <a target="_blank" href="/sponsor_packet.pdf">
-          sponsor packet
-        </a>
-        !
-      </p>
-      <div className="container">
-        <a
-          href="https://tailwindapp.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            alt="tailwind"
-            src={require("../images/sponsors/tailwind.png")}
-          />
-        </a>
-        <a
-          href="http://www.flywheelenergy.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            alt="flywheel energy"
-            src={require("../images/sponsors/flywheelEnergy.png")}
-          />
-        </a>
-        <a
-          href="https://www.paycom.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img alt="paycom" src={require("../images/sponsors/paycom.png")} />
-        </a>
-        <a
-          href="https://www.ou.edu/alumni/saa"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img alt="saa" src={require("../images/sponsors/saa.png")} />
-        </a>
-        <a
-          href="https://www.ou.edu/sga"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img alt="sga" src={require("../images/sponsors/sga.png")} />
-        </a>
-        <a
-          href="https://www.hobbylobby.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            alt="hobby lobby"
-            src={require("../images/sponsors/hobbyLobby.png")}
-          />
-        </a>
+  <div>
+    <Section>
+      <div id="sponsors" className="sponsors">
+        <h2>Sponsors</h2>
+        <p>
+          Interested in sponsoring? Check out our{" "}
+          <a target="_blank" href="/sponsor_packet.pdf">
+            sponsor packet
+          </a>
+          !
+        </p>
+        <div className="container">
+          <a
+            href="https://tailwindapp.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt="tailwind"
+              src={require("../images/sponsors/tailwind.png")}
+            />
+          </a>
+          <a
+            href="http://www.flywheelenergy.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt="flywheel energy"
+              src={require("../images/sponsors/flywheelEnergy.png")}
+            />
+          </a>
+          <a
+            href="https://www.paycom.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img alt="paycom" src={require("../images/sponsors/paycom.png")} />
+          </a>
+          <a
+            href="https://www.ou.edu/alumni/saa"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img alt="saa" src={require("../images/sponsors/saa.png")} />
+          </a>
+          <a
+            href="https://www.ou.edu/sga"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img alt="sga" src={require("../images/sponsors/sga.png")} />
+          </a>
+          <a
+            href="https://www.hobbylobby.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt="hobby lobby"
+              src={require("../images/sponsors/hobbyLobby.png")}
+            />
+          </a>
+        </div>
       </div>
-    </div>
-  </Section>
+    </Section>
+    <Section>
+      <div id="partners" className="sponsors">
+        <h2>Partners</h2>
+        <div className="container">
+          <a
+            href="https://www.stickermule.com/unlock?ref_id=0539570701&utm_source=sponsorship&utm_campaign=mlh-sponsorship-2019&utm_medium=referral"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt="stickermule"
+              src={require("../images/sponsors/stickerMule.png")}
+            />
+          </a>
+          <a href="https://mlh.io" target="_blank" rel="noopener noreferrer">
+            <img alt="mlh" src={require("../images/sponsors/mlh.png")} />
+          </a>
+        </div>
+      </div>
+    </Section>
+  </div>
 );
 
 export default Sponsors;
