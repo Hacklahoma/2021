8eae9c5390ca29854a0dce2c69fbd5b00b83c103
s-fletcher <sfletchertx@gmail.com>
2021-01-20 17:21:10 -0600

    Update CTA links

--- src/live/CTA.jsx
+++ src/live/CTA.jsx
@@ -11,15 +11,15 @@ const CTA = () => (
       {/* eslint-disable-next-line max-len */}
       <div className="step">
         <h2>1. </h2>
-        <p className="instruct">Join our Discord Server and follow instructions to link your application to check in.</p>
+        <p className="instruct">Join our <a href="/" target="_blank" rel="noopener noreferrer">Discord Server</a> and follow instructions to link your application to check in.</p>
       </div>
       <div className="step">
         <h2>2. </h2>
-        <p className="instruct">Register to our Devpost so that you can submit your project and be judged.</p>
+        <p className="instruct">Register to our <a href="https://hacklahoma21.devpost.com" target="_blank" rel="noopener noreferrer">Devpost</a> so that you can submit your project and be judged.</p>
       </div>
       <div className="step">
         <h2>3. </h2>
-        <p className="instruct">Read the Hacker Handbook to guide you through Hacklahoma 2021!</p>
+        <p className="instruct">Read the <a href="/" target="_blank" rel="noopener noreferrer">Hacker Handbook</a> to guide you through Hacklahoma 2021!</p>
       </div>
     </div>
   </div>
