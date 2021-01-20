63b094d26bf7f47bae7fa02b0e538c77f0a717a3
s-fletcher <sfletchertx@gmail.com>
2021-01-20 17:04:00 -0600

    Better countdown

--- src/live/Header.jsx
+++ src/live/Header.jsx
@@ -5,31 +5,65 @@ import '../styles/header.scss';
  * Contains all the header needs
  */
 const Header = () => {
-  const [countdown, setCountdown] = useState({ days: 0, hours: 0, mins: 0, secs: 0 });
   // Feb 6, 2021 @ 12:00pm CST
+  const startDate = new Date('2021-02-06T18:00:00Z');
+  // 24hrs after start date
+  const endDate = new Date(startDate.getTime() + 24 * 60 * 60 * 1000);
+
+  const [tick, setTick] = useState(0);
+  const [countdown, setCountdown] = useState({ days: -1, hours: -1, mins: -1, secs: -1 });
+  const [hacking, setHacking] = useState(new Date() > startDate);
+
+  /**
+   * Updates countdown
+   */
+  const updateCountdown = () => {
+    const now = new Date();
+    let date;
+    if (hacking) {
+      date = endDate;
+    } else {
+      date = startDate;
+    }
+    const dis = date - now;
+    const days = Math.floor(dis / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((dis % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((dis % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((dis % (1000 * 60)) / 1000);
+    if (dis > 0) {
+      setCountdown({ days, hours, mins, secs });
+    } else {
+      setCountdown({ days: 0, hours: 0, mins: 0, secs: 0 });
+    }
+  };
+
+  /**
+   * Generates a random tick
+   */
+  const regenerateTick = () => {
+    setTick(Math.random());
+  };
 
   useEffect(() => {
-    const startDate = new Date('2021-02-06T18:00:00Z');
-
-    /**
-     * Updates countdown
-     */
-    const updateCountdown = () => {
-      const now = new Date();
-      const dis = startDate - now;
-      const days = Math.floor(dis / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((dis % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const mins = Math.floor((dis % (1000 * 60 * 60)) / (1000 * 60));
-      const secs = Math.floor((dis % (1000 * 60)) / 1000);
-      if (dis > 0) {
-        setCountdown({ days, hours, mins, secs });
-      }
-    };
+    if (
+      !hacking
+      && countdown.days === 0
+      && countdown.hours === 0
+      && countdown.mins === 0
+      && countdown.secs === 0
+    ) {
+      setHacking(true);
+    }
 
     updateCountdown();
+  }, [tick]);
+
+  useEffect(() => {
+    updateCountdown();
+    regenerateTick();
 
     setInterval(() => {
-      updateCountdown();
+      regenerateTick();
     }, 1000);
   }, []);
 
@@ -45,13 +79,18 @@ const Header = () => {
         <div className="tv">
           <div className="content">
             <div className="container">
-              <h1>HACKING BEGINS IN...</h1>
+              <h1>HACKING {hacking ? 'ENDS' : 'BEGINS'} IN...</h1>
               <div className="countdown">
-                <div className="time">
-                  <h2>{format(countdown.days)}</h2>
-                  <p>days</p>
-                </div>
-                <h2 className="separator">:</h2>
+                {!hacking
+                && (
+                  <>
+                    <div className="time">
+                      <h2>{format(countdown.days)}</h2>
+                      <p>days</p>
+                    </div>
+                    <h2 className="separator">:</h2>
+                  </>
+                )}
                 <div className="time">
                   <h2>{format(countdown.hours)}</h2>
                   <p>hours</p>
