c4746574dfff1da3370e6664dc5aac5de44f69af
s-fletcher <sfletchertx@gmail.com>
2021-01-23 14:15:45 -0600

    Add dry run stream

--- src/live/LiveStream.jsx
+++ src/live/LiveStream.jsx
@@ -6,13 +6,13 @@ import '../styles/live.scss';
  */
 const LiveStream = () => (
   <div id="live-stream" className="live-stream">
-    {/* <iframe
+    <iframe
       title="Hacklahoma Live Stream"
-      src="YOUTUBE_LIVESTREAM_GOES_HERE"
+      src="https://www.youtube.com/watch?v=17idkokF28I"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
-    /> */}
+    />
   </div>
 );
 
