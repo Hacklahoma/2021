d41f65522e3a0f2e6b6f5f9a69b6feba8d5cefa7
s-fletcher <sfletchertx@gmail.com>
2021-02-06 10:03:43 -0600

    Add live stream

--- src/live/LiveStream.jsx
+++ src/live/LiveStream.jsx
@@ -6,13 +6,13 @@ import '../styles/live.scss';
  */
 const LiveStream = () => (
   <div id="live-stream" className="live-stream">
-    {/* <iframe
+    <iframe
       title="Hacklahoma Live Stream"
-      src="PASTE EMBED LINK HERE"
+      src="https://www.youtube.com/embed/PSTAnfcfDIo"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
-    /> */}
+    />
   </div>
 );
 
