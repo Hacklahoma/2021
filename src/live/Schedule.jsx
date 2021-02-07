4e09aa1837efc243fe1f56ad6878fbdc38a1e7df
s-fletcher <sfletchertx@gmail.com>
2021-02-07 01:48:34 -0600

    Clarify submissions due

--- src/live/Schedule.jsx
+++ src/live/Schedule.jsx
@@ -1,108 +1,108 @@
-import React, { useEffect, useState } from "react";
-import "../styles/faq.scss";
-import AnimateHeight from "react-animate-height";
-import Section from "../components/Section";
+import React, { useEffect, useState } from 'react';
+import '../styles/faq.scss';
+import AnimateHeight from 'react-animate-height';
+import Section from '../components/Section';
 
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
 ];
 const months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
 ];
 
 export const times = [
   {
-    name: "Hacker Check-In",
-    time: new Date("2021-02-06T16:00:00Z"),
+    name:     'Hacker Check-In',
+    time:     new Date('2021-02-06T16:00:00Z'),
     duration: 60,
   },
   {
-    name: "Opening ceremony",
-    time: new Date("2021-02-06T17:00:00Z"),
+    name:     'Opening ceremony',
+    time:     new Date('2021-02-06T17:00:00Z'),
     duration: 60,
   },
   {
-    name: "Hacking begins!",
-    time: new Date("2021-02-06T18:00:00Z"),
+    name:     'Hacking begins!',
+    time:     new Date('2021-02-06T18:00:00Z'),
     duration: 5,
   },
   {
-    name: "Team-building activity",
-    time: new Date("2021-02-06T18:30:00Z"),
+    name:     'Team-building activity',
+    time:     new Date('2021-02-06T18:30:00Z'),
     duration: 30,
   },
   {
-    name: "React Workshop with Mark Glose by Paycom",
+    name:        'React Workshop with Mark Glose by Paycom',
     description:
-      "React has become the leading choice for a frontend framework by companies everywhere. Come learn the basics of React and APIs as we build a simple website that interfaces with a JSON powered backend",
-    time: new Date("2021-02-06T19:15:00Z"),
+      'React has become the leading choice for a frontend framework by companies everywhere. Come learn the basics of React and APIs as we build a simple website that interfaces with a JSON powered backend',
+    time:     new Date('2021-02-06T19:15:00Z'),
     duration: 60,
   },
   {
-    name: "Build a Cloud Connected AR/VR App in 15 Minutes or Less by echoAR",
+    name:        'Build a Cloud Connected AR/VR App in 15 Minutes or Less by echoAR',
     description:
-      "The workshop will show how to quickly create AR/VR apps with no technical skills or coding required and scale existing apps by connecting them to the cloud. Workshop participants will learn how to create real-time cloud-connected augmented and virtual reality (AR/VR) apps using the echoAR platform. Participants are encouraged to bring a laptop and a smartphone, or just follow along the live demonstration. Platforms for experimentation include: Google ARCore, WebXR, Vuforia, Unity-based apps, and more.",
-    time: new Date("2021-02-06T20:30:00Z"),
+      'The workshop will show how to quickly create AR/VR apps with no technical skills or coding required and scale existing apps by connecting them to the cloud. Workshop participants will learn how to create real-time cloud-connected augmented and virtual reality (AR/VR) apps using the echoAR platform. Participants are encouraged to bring a laptop and a smartphone, or just follow along the live demonstration. Platforms for experimentation include: Google ARCore, WebXR, Vuforia, Unity-based apps, and more.',
+    time:     new Date('2021-02-06T20:30:00Z'),
     duration: 60,
   },
   {
-    name: "HTML Workshop with Jennifer Pham by Hacklahoma",
+    name:        'HTML Workshop with Jennifer Pham by Hacklahoma',
     description:
-      "In this workshop, attendees will be building and deploying a website using HTML and CSS! We will be teaching the very basics of HTML and CSS, go into detail about some terminology, and provide skeleton code to get you started. This step-by-step tutorial is meant for beginners with little to no experience with HTML and CSS.",
-    time: new Date("2021-02-06T21:45:00Z"),
+      'In this workshop, attendees will be building and deploying a website using HTML and CSS! We will be teaching the very basics of HTML and CSS, go into detail about some terminology, and provide skeleton code to get you started. This step-by-step tutorial is meant for beginners with little to no experience with HTML and CSS.',
+    time:     new Date('2021-02-06T21:45:00Z'),
     duration: 60,
   },
   {
-    name: "Design Thinking Workshop with Michael Hollinger",
+    name:        'Design Thinking Workshop with Michael Hollinger',
     description:
-      "The goal of design thinking is to keep the customer at the forefront of every product. Come learn about design thinking and see why major companies are switching to this mindset when pushing new products and projects.",
-    time: new Date("2021-02-06T23:00:00Z"),
+      'The goal of design thinking is to keep the customer at the forefront of every product. Come learn about design thinking and see why major companies are switching to this mindset when pushing new products and projects.',
+    time:     new Date('2021-02-06T23:00:00Z'),
     duration: 60,
   },
   {
-    name: "Cybersec and Bob Ross MS Paint by MLH",
+    name:        'Cybersec and Bob Ross MS Paint by MLH',
     description:
-      "Are you up to the challenge? Join MLH in a Capture the Flag activity sponsored by the US Airforce. This activity will show you the basics and help you build experience to become a master of cybersecurity. Afterwards, kick back and destress with a Bob Ross MS Paint activity. Follow along with Bob as you try to recreate what he does on screen in MS Paint!",
-    time: new Date("2021-02-07T00:15:00Z"),
+      'Are you up to the challenge? Join MLH in a Capture the Flag activity sponsored by the US Airforce. This activity will show you the basics and help you build experience to become a master of cybersecurity. Afterwards, kick back and destress with a Bob Ross MS Paint activity. Follow along with Bob as you try to recreate what he does on screen in MS Paint!',
+    time:     new Date('2021-02-07T00:15:00Z'),
     duration: 60,
   },
   {
-    name: "Online Escape Room",
+    name:        'Online Escape Room',
     description:
-      "If you love puzzles and challenges, this activity is the one for you. Come and try to figure your way out of a room by sifting through different online documents and gathering clues until you’re able to make your escape.",
-    time: new Date("2021-02-07T02:00:00Z"),
+      'If you love puzzles and challenges, this activity is the one for you. Come and try to figure your way out of a room by sifting through different online documents and gathering clues until you’re able to make your escape.',
+    time:     new Date('2021-02-07T02:00:00Z'),
     duration: 60,
   },
   {
-    name: "Jackbox!",
+    name:        'Jackbox!',
     description:
-      "Who doesn’t love video games? Come join in on the fun and play some Jackbox with your fellow hackers. We guarantee that it will be a great time.",
-    time: new Date("2021-02-07T04:00:00Z"),
+      'Who doesn’t love video games? Come join in on the fun and play some Jackbox with your fellow hackers. We guarantee that it will be a great time.',
+    time:     new Date('2021-02-07T04:00:00Z'),
     duration: 60,
   },
   {
-    name: "Cooking Show",
+    name:        'Cooking Show',
     description: (
       <p>
         <strong>Ingredients / tools</strong> <br />
-        <strong style={{ color: "green" }}>It's time to cook Mochi!</strong>
+        <strong style={{ color: 'green' }}>It's time to cook Mochi!</strong>
         <br />
         - Sticky rice
         <br /> - Water to soak rice (20% lessamount of sticky rice); recommend
@@ -137,17 +137,17 @@ export const times = [
         <br /> 5. Done
       </p>
     ),
-    time: new Date("2021-02-07T06:00:00Z"),
+    time:     new Date('2021-02-07T06:00:00Z'),
     duration: 60,
   },
   {
-    name: "Hacking ends!",
-    time: new Date("2021-02-07T18:00:00Z"),
+    name:     'Hacking ends / Submissions due!',
+    time:     new Date('2021-02-07T18:00:00Z'),
     duration: 5,
   },
   {
-    name: "Closing ceremony",
-    time: new Date("2021-02-07T18:45:00Z"),
+    name:     'Closing ceremony',
+    time:     new Date('2021-02-07T18:45:00Z'),
     duration: 30,
   },
 ];
@@ -168,7 +168,7 @@ const Schedule = () => {
    */
   const handleClick = (e) => {
     // Get index of time that was clicked
-    const name = e.currentTarget.parentNode.getAttribute("name");
+    const name = e.currentTarget.parentNode.getAttribute('name');
     const timeObj = times.find((val) => val.name === name);
     const index = times.indexOf(timeObj);
 
@@ -185,11 +185,11 @@ const Schedule = () => {
   const formatTime = (time) => {
     let m = time.getMinutes();
     m = `0${m}`.slice(-2);
-    let dd = "AM";
+    let dd = 'AM';
     let h = time.getHours();
     if (h >= 12) {
       h -= 12;
-      dd = "PM";
+      dd = 'PM';
     }
     if (h === 0) {
       h = 12;
@@ -203,16 +203,15 @@ const Schedule = () => {
   function getTimezoneName() {
     const today = new Date();
     const short = today.toLocaleDateString(undefined);
-    const full = today.toLocaleDateString(undefined, { timeZoneName: "short" });
+    const full = today.toLocaleDateString(undefined, { timeZoneName: 'short' });
 
     // Trying to remove date from the string in a locale-agnostic way
     const shortIndex = full.indexOf(short);
     if (shortIndex >= 0) {
-      const trimmed =
-        full.substring(0, shortIndex) +
-        full.substring(shortIndex + short.length);
+      const trimmed = full.substring(0, shortIndex)
+        + full.substring(shortIndex + short.length);
 
-      return trimmed.replace(/^[\s,.\-:;]+|[\s,.\-:;]+$/g, "");
+      return trimmed.replace(/^[\s,.\-:;]+|[\s,.\-:;]+$/g, '');
     }
 
     return full;
@@ -229,16 +228,16 @@ const Schedule = () => {
     // Iterate through all scheduled times
     times.forEach((val, i) => {
       let day;
-      let classes = "item";
+      let classes = 'item';
 
       // Setup classes
       if (
-        val.time < now &&
-        now.getTime() < val.duration * 60000 + val.time.getTime()
+        val.time < now
+        && now.getTime() < val.duration * 60000 + val.time.getTime()
       ) {
-        classes += " ongoing";
+        classes += ' ongoing';
       } else if (val.time < now) {
-        classes += " completed";
+        classes += ' completed';
       }
 
       // Check if a day header has been created
@@ -254,7 +253,7 @@ const Schedule = () => {
         results.push(
           <h3 key={`${val.name}-header`}>
             {day}, {months[val.time.getMonth()]} {val.time.getDate()}
-          </h3>
+          </h3>,
         );
       }
 
@@ -268,14 +267,14 @@ const Schedule = () => {
                 <span>{val.name}</span>
               </p>
             </div>
-          </div>
+          </div>,
         );
       } else {
         // Push an expandable schedule item that has a description
         results.push(
           <div
             key={val.name}
-            className={`${classes}${items[i] ? " show" : ""}`}
+            className={`${classes}${items[i] ? ' show' : ''}`}
             name={val.name}
           >
             <button onClick={handleClick}>
@@ -285,12 +284,12 @@ const Schedule = () => {
               </p>
               <ExpandIcon />
             </button>
-            <AnimateHeight height={`${items[i] ? "auto" : "0%"}`}>
+            <AnimateHeight height={`${items[i] ? 'auto' : '0%'}`}>
               <div className="answer">
                 <p>{val.description}</p>
               </div>
             </AnimateHeight>
-          </div>
+          </div>,
         );
       }
     });
